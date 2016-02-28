cb956c355a9c9ecc661af0cb54d6b3a49e5bc2ed
Nikita <keddofilatov@gmail.com>
2016-02-28 18:30:16 +0600

    Create article#edit action and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,21 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
