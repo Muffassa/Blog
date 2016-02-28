0a95c908ec44b3b298e9cda430c0a0012ef41148
Nikita <keddofilatov@gmail.com>
2016-02-28 13:00:01 +0600

    Add action article#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def new
   end
+
   def create
     @article = Article.new(article_params)
 
@@ -8,6 +9,9 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
   private
     def article_params
       params.require(:article).permit(:title, :text)
